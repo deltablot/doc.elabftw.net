4476c625c453cd048f670e4b336533d9250e6a64
Nicolas CARPi <nico-git@deltablot.email>
2020-05-30 18:07:41 +0200

    add favicon.ico

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ docker build --no-cache -t elabftw/elabdoc-builder .
 cid=$(docker create elabftw/elabdoc-builder)
 docker cp "$cid:/home/node/build/doc" build
 docker rm "$cid"
+# add favicon.ico
+cp favicon.ico build
