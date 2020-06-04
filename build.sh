43c2758b5a1061bbf967ade9c82491e3fe02971a
Nicolas CARPi <nico-git@deltablot.email>
2020-06-04 22:25:37 +0200

    mv build to build.old

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 docker build --no-cache -t elabftw/elabdoc-builder .
 cid=$(docker create elabftw/elabdoc-builder)
+mv build build.old
 docker cp "$cid:/home/node/build/doc" build
 docker rm "$cid"
 # add favicon.ico
 cp favicon.ico build
+rm -r build.old
